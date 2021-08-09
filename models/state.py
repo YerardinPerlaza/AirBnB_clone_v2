b6193bf1151d0094b6e0701d048b9ae469ddc5c0
YerardinPerlaza <yerardinpm@gmail.com>
2021-08-09 16:51:22 +0000

    add getter to state

--- models/state.py
+++ models/state.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="delete")
+
+    @property
+    def cities(self):
+        city_dict = models.storage.all(City)
+        city_list = []
+        for key, val in city_dict.items():
+            if val.state_id == self.id:
+                city_list.append(val)
+        return city_list
