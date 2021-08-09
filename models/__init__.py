85931e13417ef407142915a0ba694077c51bf5fb
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-09 23:38:46 +0000

    Add: implementation of db_storage with state and user, working create and all in console for db

--- models/__init__.py
+++ models/__init__.py
@@ -2,10 +2,10 @@
 """This module instantiates an object of class FileStorage"""
 from os import getenv
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    # from models.engine.db_storage import DBStorage
-    # storage = DBStorage()
-    pass
+env = getenv('HBNB_TYPE_STORAGE')
+if env == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
--- models/state.py
+++ models/state.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from models.city import City
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="delete")
 
-    @property
-    def cities(self):
-        city_dict = models.storage.all(City)
-        city_list = []
-        for key, val in city_dict.items():
-            if val.state_id == self.id:
-                city_list.append(val)
-        return city_list
+    if models.env == 'db':
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            city_dict = models.storage.all(City)
+            city_list = []
+            for key, val in city_dict.items():
+                if val.state_id == self.id:
+                    city_list.append(val)
+            return city_list
