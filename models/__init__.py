7fcccef256cc78583103ea2c8c278e935d799a62
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2021-08-10 03:34:40 +0000

    Add more documentation for __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage
+depending on the HBNB_TYPE_STORAGE environment variable"""
 from os import getenv
 
 env = getenv('HBNB_TYPE_STORAGE')
